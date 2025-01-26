a58b34414b53a1113432d995c3c3032454bfa0aa
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2025-01-26 12:20:22 +0300

    input and str methods

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
-print("Hello from Pycharm")
-print('alex', '10', True)
-print(dir(__builtins__))
+# print("Hello from Pycharm")
+# print('alex', '10', True)
+# print(dir(__builtins__))
+
+name = input('Enter your name: ')
+age = input('Enter your age: ')
+city = input('Enter your city: ')
+print(name, age, city)
+
+print(name.capitalize())
+# Возвращает в ответе имя с заглавной буквы
+print(name.upper())
+# Возвращает в ответе имя заглавными буквами
+print(dir(name))
