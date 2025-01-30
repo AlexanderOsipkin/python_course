b697c993ad7737e05fef778df5eb7fae6f954f04
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2025-01-30 14:42:44 +0300

    methods

---
+++ methods.py
@@ -0,0 +1,11 @@
+my_comment = "This is my comment"
+
+print(len(my_comment))
+
+print(my_comment.replace('comment', 'long comment'))
+
+print(my_comment)
+
+print(my_comment.count('m'))
+
+print(my_comment[0:8])
