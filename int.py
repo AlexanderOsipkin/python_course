a454e5767edc74b605ed79c34a26943573f1acef
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2025-01-30 14:50:49 +0300

    int

---
+++ int.py
@@ -0,0 +1,12 @@
+input_str = input('Enter any number: ')
+input_int = int(input_str)
+
+print(input_int)
+
+print(type(input_int))
+
+a = 4
+b = 6
+pow_num = pow(a, b)
+print(pow_num)
+print(type(pow_num))
