b8e56cbcf7233f7f894ff69a01e345b7758f0052
AlexanderOsipkin <aleksandrosipkin@yandex.ru>
2025-01-30 15:02:19 +0300

    bool

---
+++ bool.py
@@ -0,0 +1,15 @@
+db_is_available = False
+
+print(db_is_available)
+print(type(db_is_available))
+
+db_is_available = True
+
+print(db_is_available)
+
+print(bool(10))
+print(bool([1, 2, 3]))
+print(100 > 10)
+print('long str' > 'long')
+print([10] == [20])
+print({'a': 3} == {'a': 5})
